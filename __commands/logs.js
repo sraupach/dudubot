e4dd98284a6d4e81fc4d448cb4e21e428611a2b4
sraupach <sraupach@gehma.eu>
2017-04-07 15:29:28 +0200

    integrated LastLog feat

--- __commands/logs.js
+++ __commands/logs.js
@@ -1,16 +1,54 @@
 //warcraftlogs command
 module.exports = function command(bot, container) {
+    const util = container.util
+    // dependancies------------------------------
+    const warcraftlogs = util.usePlugin('warcraftlogs');
+    if (!warcraftlogs) {
+        return;
+    }
     // command-----------------------------------
     return {
         alias: ['l'],
         description: 'Get some warcraftlogs',
         permissions: 'public',
-        action: function(meta) {
+        action: function (meta) {
+            //warcraftlogs.getInstance(meta.input, function (result) {
+            warcraftlogs.getLastLog(meta.input, function (result) {
+                // ZONE should be >> warcraftlogs.getInstance(result[0].zone) <<
+                //let line = result[0].title + ' Uploaded by: ' + result[0].owner + ' in ' + result[0].zone
+                var Instance = warcraftlogs.getInstance(result[0].zone);
+                let embedObj = {
+                    type: 'rich',
+                    title: 'Warcraftlogs',
+                    description: 'Latest <in Harmony> log',
+                    color: container.util.toColorInt('01DFD7'),
+                    fields: [],
+                    thumbnail: {
+                        url: 'https://www.warcraftlogs.com/img/common/warcraft-logo.png',
+                        proxy_url: '',  // optional
+                        height: '25px', // optional
+                        width: '25px'   // optional
+                    }
+                };
+                // Add logInfo
+                embedObj.fields.push({
+                    name: result[0].title,
+                    value: '[Direct Log](https://www.warcraftlogs.com/reports/' + result[0].id + ')',
+                    inline: true
+                });
+                // Add LogLocation
+                embedObj.fields.push({
+                    name: 'Instance',
+                    value: ''+Instance+'',
+                    inline: true
+                });
 
                 bot.sendMessage({
                     to: meta.channelID,
-                    message: 'This Module is under construction'
+                    message: 'This Module is under construction\n',
+                    embed: embedObj
                 });
+            });
 
         }
     };
--- __plugins/warcraftlogs.js
+++ __plugins/warcraftlogs.js
@@ -17,18 +17,49 @@ module.exports = function plugin(bot, container, pconfig) {
     api.setApiKey(pconfig.api);
     // plugin------------------------------------
     // new module for counting the users in channel
-    plugin.getInstance = function (name) {
+    plugin.getInstance = function (inputid) {
         api.getZones(function (err, data) {
             //console.log(data["id"].name);
-            let lines = '';
+
+            let lines = 'default';
             Object.keys(data).forEach(function (id) {
-                if (data[id].id === name) {
-                    lines += id + ' Zone-ID: ' + data[id].id + ' -> ' + data[id].name + '\n';
+                if (data[id].id === inputid) {
+                    lines = id + ' Zone-ID: ' + data[id].id + ' -> ' + data[id].name + '\n';
+                    console.log(data[id].name);
+                    return data[id].name
+                    
                 }
             });
+
         });
-        return lines;
     }
+    plugin.getLastLog = function (userinput, callback) {
+        var guild = "in Harmony";
+        var realm = "Blackhand";
+        console.log('into getLastLog Module')
+        //Params
+        var lastlog = {};
+        var OneWeek = 604800000;
+        //Optional parameters for the api call. 
+        var params = { start: Date.now() - OneWeek };
+
+        //Call the function to list guild reports, can be filtered on start time and end time as a UNIX timestamp with the optional parameters @params. 
+        api.getReportsGuild(guild, realm, 'eu', params, function (err, data) {
+
+            if (err) {
+                //We caught an error, log the error object to the console and exit. 
+                console.log(err);
+                return;
+            }
+            //Success, log the whole data object to the console. 
+            console.log(data);
+            callback(data);
+        });
+
+    }
+
+
+
 
     return plugin;
 };
